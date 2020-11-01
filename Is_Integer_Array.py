09616b16323be46d397b9d9036c3322801b7b714
Reda BELHAJ <58605073+Reda-BELHAJ@users.noreply.github.com>
2020-11-01 17:07:41 +0100

    Update Is_Integer_Array.py

--- Is_Integer_Array.py
+++ Is_Integer_Array.py
@@ -1,5 +1,3 @@
-from typing import List, Any
-
-def is_int_array(arr: List[Any]):
+def is_int_array(arr):
     return isinstance(arr, list) and all(isinstance(n, (int, float)) 
                                          and n - int(n) == 0 for n in arr)
